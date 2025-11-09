7e84a786201e8305ddfe8dd182a051562ed9e4d1
Tamara McG <tamaramcgiffin@gmail.com>
2025-11-09 15:17:21 -0800

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,52 @@
+// ---------------------------------
+// Boilerplate Code to Set Up Server
+// ---------------------------------
+
+// ---------------------------------
+// Helper Functions
+// ---------------------------------
+
+// 1. getAllAnimals()
+
+// 2. getOneAnimalByName(name)
+
+// 3. getOneAnimalById(id)
+
+// 4. getNewestAnimal()
+
+// 5. getAllMammals()
+
+// 6. getAnimalsByCategory(category)
+
+// 7. deleteOneAnimal(id)
+
+// 8. addOneAnimal(name, category, can_fly, lives_in)
+
+// 9. updateOneAnimalName(id, newName)
+
+// 10. updateOneAnimalCategory(id, newCategory)
+
+
+// ---------------------------------
+// API Endpoints
+// ---------------------------------
+
+// 1. GET /get-all-animals
+
+// 2. GET /get-one-animal-by-name/:name
+
+// 3. GET /get-one-animal-by-id/:id
+
+// 4. GET /get-newest-animal
+
+// 5. GET /get-all-mammals
+
+// 6. GET /get-animals-by-category/:category
+
+// 7. POST /delete-one-animal/:id
+
+// 8. POST /add-one-animal
+
+// 9. POST /update-one-animal-name
+
+// 10. POST /update-one-animal-category
